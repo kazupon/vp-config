b2c47d4edf70c32ab6df3ba58459c2e2ea73aafa
kazuya kawaguchi <kawakazu80@gmail.com>
2026-03-15 21:44:35 +0900

    revert

--- src/format.ts
+++ src/format.ts
@@ -1,13 +1,13 @@
 // TODO: workaround for vite-plus-test DTS bundling issue (missing ./aria-role.js)
-import type { UserConfig } from 'vite-plus'
-const config: UserConfig['fmt'] = {
-  // const config: {
-  //   semi?: boolean
-  //   singleQuote?: boolean
-  //   trailingComma?: 'all' | 'none'
-  //   endOfLine?: 'lf' | 'crlf' | 'cr'
-  //   arrowParens?: 'always' | 'avoid'
-  // } = {
+// import type { UserConfig } from 'vite-plus'
+// const config: UserConfig['fmt'] = {
+const config: {
+  semi?: boolean
+  singleQuote?: boolean
+  trailingComma?: 'all' | 'none'
+  endOfLine?: 'lf' | 'crlf' | 'cr'
+  arrowParens?: 'always' | 'avoid'
+} = {
   semi: false,
   singleQuote: true,
   trailingComma: 'none',
