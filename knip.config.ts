1769d31ebe5730a66a63a105efef4240ca38f4e0
kazuya kawaguchi <kawakazu80@gmail.com>
2026-06-08 19:48:53 +0900

    fix

--- knip.config.ts
+++ knip.config.ts
@@ -1,11 +1,17 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
+  entry: ['src/index.ts', 'tests/**/*.test.ts'],
   ignoreDependencies: [
     '@kazupon/eslint-plugin',
     '@ox-jsdoc/eslint-plugin-jsdoc',
-    'pkg-pr-new',
-    'vitest'
+    '@oxc-project/types',
+    '@types/chai',
+    '@types/deep-eql',
+    'assertion-error',
+    'oxfmt',
+    'oxlint',
+    'pkg-pr-new'
   ]
 }
 
